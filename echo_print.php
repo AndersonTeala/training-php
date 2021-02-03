03ce20a60175697a5cd4545be652d4dc09986f10
AndersonTeala <andersonteala@gmail.com>
2021-02-03 15:35:33 -0300

    First commit

---
+++ echo_print.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Anderson Teala</title>
+	</head>
+	<body>
+		<?php
+			echo 'Comando echo<br/>';
+			print 'Comando print<hr/>';
+		?>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Anderson Teala</title>
+	</head>
+
+	<body>
+		<?php
+			echo "Hello World <hr/>";
+		?>
+
+	</body>
+</html>
