db5463e9b2581ed19aa6072330d89ec803b8d736
Anderson Teala <andersonteala@gmail.com>
2021-02-26 13:25:54 -0300

    Learning mathematical functions

---
+++ www/funcoes_matematicas.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="styles.css">
+		<title>Anderson Teala</title>
+	</head>
+	<body>
+
+		<?php
+
+               //$num = 8.3;
+               //echo ceil($num) // arredonda para um num inteiro pra cima
+               //echo floor ($num) // arredonda o valor para baixo
+               //echo round ($num) // arredonda com base na fraçao
+               //echo rand(1, 3); //gerar um valor aleatório
+               //echo '<br/>' . getrandmax(); //saber o valor máximo para gerar
+               echo sqrt(9120) //retorna a raiz quadrada
+
+		?>
+
+	</body>
+</html>
