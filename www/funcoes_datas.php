9b0e7b9fe9d05c064e2bd40d3ca27ca79b80d54f
Anderson Teala <andersonteala@gmail.com>
2021-03-01 12:21:47 -0300

    Creating functions with dates

--- www/funcoes_datas.php
+++ www/funcoes_datas.php
@@ -9,14 +9,37 @@
 
 		<?php
 
-				//recuperação da data atual / data corrente
+			/* 	//recuperação da data atual / data corrente
 				echo date_default_timezone_set('America/Sao_Paulo');
 				echo '<br/>';
 				echo date('d/m/Y H:i');
 
 				//
 				echo '<br/>';
-				echo date('d/m/Y H:i');
+				echo date('d/m/Y H:i'); */
+
+				$data_inicial = '1996-01-23';
+				$data_final = '2021-03-01';
+
+				//timestamp - 01/01/1070 -- 2018-04-24 (js->milissegundos / php->segundos)
+				$time_inicial = strtotime($data_inicial);
+				$time_final = strtotime($data_final);
+				echo $data_inicial . ' ' . $time_inicial;
+				echo '<br/>';
+				echo $data_final . ' ' . $time_final;
+
+				//$diferenca_times = $time_final - $time_inicial;
+				$diferenca_times = abs($time_inicial - $time_final); //abs
+				echo '<br/>';
+				echo 'A diferença de segundos entre a data incial e final é ' . $diferenca_times;
+
+				$segundos_existem_dia = 24 * 60 * 60;
+				echo '<br/>';
+				echo 'Um dia possui ' . $segundos_existem_dia . ' Segundos';
+				$diferenca_de_dias_entre_as_datas = $diferenca_times / $segundos_existem_dia;
+				echo '<br/>';
+				echo 'A diferença em dias é: ' . $diferenca_de_dias_entre_as_datas;
+
 
 		?>
 
