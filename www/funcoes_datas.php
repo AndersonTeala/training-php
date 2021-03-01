450f2970869d08cf7f046ef9bd32db94a3a5c91e
Anderson Teala <andersonteala@MacBook-Pro-de-Anderson.local>
2021-03-01 12:01:25 -0300

    Creating functions with dates

---
+++ www/funcoes_datas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="styles.css">
+		<title>Anderson Teala</title>
+	</head>
+	<body>
+
+		<?php
+
+				//recuperação da data atual / data corrente
+				echo date_default_timezone_set('America/Sao_Paulo');
+				echo '<br/>';
+				echo date('d/m/Y H:i');
+
+				//
+				echo '<br/>';
+				echo date('d/m/Y H:i');
+
+		?>
+
+	</body>
+</html>
